1e21d56c1ab3541b5d1b9933da1994279fd9b346
prasadk <prasadkatepk@gmail.com>
2023-12-25 18:38:30 +0530

    chore: initial setup

---
+++ src/lib/utils.ts
@@ -0,0 +1,25 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "INR";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) {
+  const { currency = "INR", notation = "compact" } = options;
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
+}
